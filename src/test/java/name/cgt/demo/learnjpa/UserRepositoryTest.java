9f8579b4511fe8224a6635af554e152f5530224f
Christoffer G. Thomsen <chris@cgt.name>
2019-09-14 17:16:34 +0200

    Expect User.id to be generated automatically
    Delete obsolete, failing test. Add new test to express the actual
    behavior.

--- src/test/java/name/cgt/demo/learnjpa/UserRepositoryTest.java
+++ src/test/java/name/cgt/demo/learnjpa/UserRepositoryTest.java
@@ -3,10 +3,8 @@ package name.cgt.demo.learnjpa;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.orm.jpa.JpaSystemException;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DataJpaTest
 public class UserRepositoryTest {
@@ -20,12 +18,12 @@ public class UserRepositoryTest {
     }
 
     @Test
-    public void save_user_fails_when_user_does_not_have_id() {
+    public void new_user_without_id_is_given_id_when_saved() {
         var user = new User();
         user.setName("");
-        assertThrows(
-            JpaSystemException.class,
-            () -> repository.save(user)
-        );
+        assertThat(user.getId()).isNull();
+
+        var savedUser = repository.save(user);
+        assertThat(savedUser.getId()).isNotNull();
     }
 }
