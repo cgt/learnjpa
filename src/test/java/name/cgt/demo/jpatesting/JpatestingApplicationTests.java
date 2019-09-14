4de1fb5dd18dfd15755b006ee02e46aac8d18ffd
Christoffer G. Thomsen <chris@cgt.name>
2019-09-14 15:23:21 +0200

    Use JUnit 5 instead of 4

--- src/test/java/name/cgt/demo/jpatesting/JpatestingApplicationTests.java
+++ src/test/java/name/cgt/demo/jpatesting/JpatestingApplicationTests.java
@@ -1,16 +1,13 @@
 package name.cgt.demo.jpatesting;
 
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(SpringRunner.class)
 @SpringBootTest
 public class JpatestingApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Test
+    public void contextLoads() {
+    }
 
 }
