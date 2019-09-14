a8095d2db1586adccdd767d82c4711f032a74b7c
Christoffer G. Thomsen <chris@cgt.name>
2019-09-14 14:49:05 +0200

    Initialize project with Spring Initializr

---
+++ src/main/java/name/cgt/demo/jpatesting/JpatestingApplication.java
@@ -0,0 +1,13 @@
+package name.cgt.demo.jpatesting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpatestingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpatestingApplication.class, args);
+	}
+
+}
